c65e5e1c631c1f14960f591c97293598248a1054
Andie Shining Phan <asphan@gmail.com>
2023-12-06 15:48:10 -0800

    Day 1: Trebuchet?! Part 2 and formatting

--- 2023/01/solution.py
+++ 2023/01/solution.py
@@ -1,5 +1,12 @@
 from pathlib import Path
 
+
+def readlines():
+    input_file = Path(__file__).parent / "input.txt"
+    with open(input_file, "r") as f:
+        return f.readlines()
+
+
 def find_first_and_last_digit(input_str):
     # Find the first digit
     first_digit = next((char for char in input_str if char.isdigit()), None)
@@ -10,20 +17,87 @@ def find_first_and_last_digit(input_str):
 
     return first_digit, last_digit
 
+
 def to_int(digits):
-    return int(''.join(digits))
+    return int("".join(digits))
 
 
 def sum_of_all_calibration_values(calibration_document):
-    return sum(to_int(find_first_and_last_digit(line))  for line in calibration_document)
+    return sum(to_int(find_first_and_last_digit(line)) for line in calibration_document)
+
+
+def find_first_and_last_occuring_substring_from_list_of_substrings(
+    input_str, substrings
+):
+    first_substring = None
+    first_index = None
+    last_substring = None
+    last_index = None
+
+    for substring in substrings:
+        if substring in input_str:
+            if first_substring is None:
+                first_substring = substring
+                first_index = input_str.index(substring)
+                last_substring = substring
+                last_index = input_str.rindex(substring)
+            else:
+                if input_str.index(substring) < first_index:
+                    first_substring = substring
+                    first_index = input_str.index(substring)
+                if input_str.rindex(substring) > last_index:
+                    last_substring = substring
+                    last_index = input_str.rindex(substring)
+    return first_substring, last_substring
+
+
+digit_value_map = {
+    "one": 1,
+    "two": 2,
+    "three": 3,
+    "four": 4,
+    "five": 5,
+    "six": 6,
+    "seven": 7,
+    "eight": 8,
+    "nine": 9,
+    "1": 1,
+    "2": 2,
+    "3": 3,
+    "4": 4,
+    "5": 5,
+    "6": 6,
+    "7": 7,
+    "8": 8,
+    "9": 9,
+}
+
+
+def sum_of_all_calibration_values_with_letter_digits(calibration_document):
+    total = 0
+    for line in calibration_document:
+        first, last = find_first_and_last_occuring_substring_from_list_of_substrings(
+            line, digit_value_map.keys()
+        )
+        total += int(f"{digit_value_map[first]}{digit_value_map[last]}")
+    return total
 
 
 example = ["1abc2", "pqr3stu8vwx", "a1b2c3d4e5f", "treb7uchet"]
-input_file = Path(__file__).parent / 'input.txt'
-
-if __name__ == '__main__':
+example_part_2 = [
+    "two1nine",
+    "eightwothree",
+    "abcone2threexyz",
+    "xtwone3four",
+    "4nineeightseven2",
+    "zoneight234",
+    "7pqrstsixteen",
+]
+if __name__ == "__main__":
     assert sum_of_all_calibration_values(example) == 142
 
-    with open(input_file, 'r') as f:
-        solution = sum_of_all_calibration_values(f.readlines())
-    assert solution == 55172
+    input_lines = readlines()
+    assert sum_of_all_calibration_values(input_lines) == 55172
+
+    assert sum_of_all_calibration_values_with_letter_digits(example_part_2) == 281
+    assert sum_of_all_calibration_values_with_letter_digits(input_lines) == 54925
--- 2023/02/solution.py
+++ 2023/02/solution.py
@@ -1,9 +1,11 @@
 from pathlib import Path
 
+
 def readlines():
-    input_file = Path(__file__).parent / 'input.txt'
-    with open(input_file, 'r') as f:
+    input_file = Path(__file__).parent / "input.txt"
+    with open(input_file, "r") as f:
         return f.readlines()
+
 
 example = """Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
 Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
@@ -11,32 +13,37 @@ Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
 Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
 Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"""
 
+
 def parse_game_record(game_record):
-    game_id, round_info = game_record[5:].split(':')
+    game_id, round_info = game_record[5:].split(":")
     rounds = []
-    for r in round_info.strip().split(';'):
+    for r in round_info.strip().split(";"):
         _round = {}
-        for count_colors in r.strip().split(','):
-            count, color = count_colors.strip().split(' ')
+        for count_colors in r.strip().split(","):
+            count, color = count_colors.strip().split(" ")
             _round.update({color: int(count)})
         rounds.append(_round)
-    return {'game_id': int(game_id), 'rounds': rounds}
+    return {"game_id": int(game_id), "rounds": rounds}
 
-game_bag = {'red': 12, 'green': 13, 'blue': 14}
+
+game_bag = {"red": 12, "green": 13, "blue": 14}
+
 
 def is_valid_game(game):
-    for round in game['rounds']:
+    for round in game["rounds"]:
         for color, count in round.items():
             if count > game_bag[color]:
                 return False
     return True
 
+
 def sum_of_valid_game_ids(games):
-    return sum(game['game_id'] for game in games if is_valid_game(game))
+    return sum(game["game_id"] for game in games if is_valid_game(game))
+
 
 def find_min_pieces_per_color_in_game(game):
     min_pieces_per_color = {}
-    for round in game['rounds']:
+    for round in game["rounds"]:
         for color, count in round.items():
             if color not in min_pieces_per_color:
                 min_pieces_per_color[color] = count
@@ -44,11 +51,13 @@ def find_min_pieces_per_color_in_game(game):
                 min_pieces_per_color[color] = max(min_pieces_per_color[color], count)
     return min_pieces_per_color
 
+
 def power_of_game_bag_set(bag):
     _power = 1
     for count in bag.values():
         _power *= count
     return _power
+
 
 def sum_power_of_game_bag_sets(games):
     sum_of_power = 0
@@ -57,8 +66,9 @@ def sum_power_of_game_bag_sets(games):
         sum_of_power += power_of_game_bag_set(min_pieces_per_color)
     return sum_of_power
 
-if __name__ == '__main__':
-    example_games = [parse_game_record(game) for game in example.split('\n')]
+
+if __name__ == "__main__":
+    example_games = [parse_game_record(game) for game in example.split("\n")]
     assert sum_of_valid_game_ids(example_games) == 8
 
     input_games = [parse_game_record(game) for game in readlines()]
